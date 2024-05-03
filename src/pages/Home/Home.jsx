46b0f6302e6609fc09bd23ecc08ec91a4ca34d9f
Md. Asif Imtiyaj Chowdhury <asifimtiyajchowdhury@gmail.com>
2024-05-04 01:08:31 +0600

    adding all the neccessary files for the site

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
