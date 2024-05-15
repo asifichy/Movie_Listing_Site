1baf97419f11c32509f26c00670cd63e66ca8643
Md. Asif Imtiyaj Chowdhury <asifniloy45@gmail.com>
2024-05-15 13:55:32 +0600

    adding api for popular movies from TMDB

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -8,6 +8,16 @@ const TitleCards = ({title, category}) => {
   //for scroll horizontally without shift key
   const cardsRef = useRef();
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjI4NDlmYTYwNTQ0NDYwOTk4ZmViY2FmZGI4MGU3OCIsInN1YiI6IjY2NDQ2NTkyMTVhNjJjODQ0OGVjM2RmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EveVMu-T_man3cxBXXYOOAenujvGNTEYkR0XsYOJZs0'
+    }
+  };
+  
+  
+
   const handleWheel = (event) => {
     // it'll not scroll the webpage (popular on netflix vertically rather than it'll scroll horizontally)
     event.preventDefault(); 
@@ -17,6 +27,12 @@ const TitleCards = ({title, category}) => {
   //effect
   // arrow function will execute whenever the component will be loaded
   useEffect(() => {
+    //fetching the api response    
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
   }, []) 
 
